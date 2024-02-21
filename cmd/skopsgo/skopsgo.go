2d9f3e8044a1ac31c90f7002a0df061c016ef8f2
Nikola Stojkov <stojkov@protonmail.ch>
2024-02-21 17:34:58 +0000

    Env config

--- cmd/skopsgo/skopsgo.go
+++ cmd/skopsgo/skopsgo.go
@@ -3,15 +3,32 @@ package main
 import (
   "net/http"
 
+  "log"
+  "fmt"
+  "os"
   "github.com/gin-gonic/gin"
+  "github.com/joho/godotenv"
 )
 
 func main() {
+  err := godotenv.Load(".env")
+
+  if err != nil {
+    log.Fatalf("Error loading .env file")
+  }
+
+  port := os.Getenv("SERVER_PORT")
+
+  if port == "" {
+    port = "8080"
+  }
+
   r := gin.Default()
   r.GET("/ping", func(c *gin.Context) {
     c.JSON(http.StatusOK, gin.H{
       "message": "pong",
     })
   })
-  r.Run(":42069")
+
+  r.Run(fmt.Sprintf(":%s", port))
 }
