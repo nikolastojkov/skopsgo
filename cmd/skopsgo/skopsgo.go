025170a36e80b33e27c9000fd834ec7bbaf689b9
Nikola Stojkov <stojkov@protonmail.ch>
2024-02-21 19:18:29 +0000

    Minor cleanup, ready for Templ

--- cmd/skopsgo/skopsgo.go
+++ cmd/skopsgo/skopsgo.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-  "github.com/nikolastojkov/skopsgo/internal/config"
-  "github.com/nikolastojkov/skopsgo/web/app"
+	"github.com/nikolastojkov/skopsgo/internal/config"
+	"github.com/nikolastojkov/skopsgo/web/app"
 )
 
 func main() {
-  config.LoadEnvConfig()
-  app.LoadApp()
+	config.LoadEnvConfig()
+	app.LoadApp()
 }
--- web/app/app.go
+++ web/app/app.go
@@ -1,20 +1,20 @@
 package app
 
 import (
-  "net/http"
-  "fmt"
-  "github.com/gin-gonic/gin"
-  "os"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"os"
 )
 
 func LoadApp() {
-  port := os.Getenv("SERVER_PORT")
-  r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "pong",
-    })
-  })
+	port := os.Getenv("SERVER_PORT")
 
-  r.Run(fmt.Sprintf(":%s", port))
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello World")
+	})
+
+	r.Run(fmt.Sprintf(":%s", port))
 }
