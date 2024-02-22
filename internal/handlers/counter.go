cd1e736a1531a1a0273d0b499fb5ac326c300154
Nikola Stojkov <stojkov@protonmail.ch>
2024-02-22 16:00:31 +0000

    correct http verb for counter

--- internal/handlers/counter.go
+++ internal/handlers/counter.go
@@ -8,12 +8,12 @@ import (
 var counterValue int = 1
 
 func loadCounterHandler(router *gin.Engine) {
-  router.POST("/counterIncrement", func(context *gin.Context) {
+  router.PUT("/counterIncrement", func(context *gin.Context) {
 		counterValue++
 		context.String(context.Writer.Status(), fmt.Sprintf("%d", counterValue))
 	})
 
-	router.POST("/counterDecrement", func(context *gin.Context) {
+	router.PUT("/counterDecrement", func(context *gin.Context) {
 		if counterValue != 1 {
 			counterValue--
 		}
