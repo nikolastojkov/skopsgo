df972c6c465f6d73ebde00a33dabe17ef82386a0
Nikola Stojkov <stojkov@protonmail.ch>
2024-02-24 17:42:23 +0000

    Router group

--- internal/handlers/counter.go
+++ internal/handlers/counter.go
@@ -8,12 +8,14 @@ import (
 var counterValue int = 1
 
 func loadCounterHandler(router *gin.Engine) {
-  router.PUT("/counterIncrement", func(context *gin.Context) {
+  counterRouter := router.Group("/counter")
+
+  counterRouter.PUT("/increment", func(context *gin.Context) {
 		counterValue++
 		context.String(context.Writer.Status(), fmt.Sprintf("%d", counterValue))
 	})
 
-	router.PUT("/counterDecrement", func(context *gin.Context) {
+	counterRouter.PUT("/decrement", func(context *gin.Context) {
 		if counterValue != 1 {
 			counterValue--
 		}
